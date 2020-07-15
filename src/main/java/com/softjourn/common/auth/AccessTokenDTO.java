d5099a7a717f35d33e24c35b5262f6684fe9c731
Orest <oguziy@softjourn.com>
2020-07-15 18:51:47 +0300

    [2.x-update] Update COINS Service to support Hyperledger 2.x network

--- src/main/java/com/softjourn/common/auth/AccessTokenDTO.java
+++ src/main/java/com/softjourn/common/auth/AccessTokenDTO.java
@@ -3,8 +3,10 @@ package com.softjourn.common.auth;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class AccessTokenDTO {
     @JsonProperty("access_token")
